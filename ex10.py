1c6a04afa5ad842d878be3b4bf4355c137ef9037
yonlu <lss.emailbox@gmail.com>
2018-05-21 13:55:43 -0400

    added ex9 ~ 12

---
+++ ex10.py
@@ -0,0 +1,31 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat ="I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+many_cats = """
+\tI'm a tabbed in cat!
+
+I'm a split \non a line cat!
+
+I'm \\ a \\ backslash \\ cat!
+"""
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+combined_cat = """
+\tLet's\n\t\tmix
+\t\t \\All\\
+\t\t\t The cats together!
+"""
+
+#print(tabby_cat)
+#print(persian_cat)
+#print(backslash_cat)
+print(many_cats)
+print(fat_cat)
+print(combined_cat)
