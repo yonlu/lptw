6271d1d1b7cf8a36b25ae5ed7a3535025dfb430d
yonlu <lss.emailbox@gmail.com>
2018-05-22 11:53:16 -0400

    added ex13m, updated ex12

---
+++ ex13-1.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script, firstArg, secondArg = argv
+
+print(f"""
+The name of the script is: {script}.
+My first variable is: {firstArg}.
+My second variable is: {secondArg}.
+"""
+)
